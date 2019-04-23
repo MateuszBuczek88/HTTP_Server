f1e25ef51f5fc7438ef651a8529843f40fd849c4
Mateusz <mateusz_poczta@o2.pl>
2019-04-23 20:51:23 +0200

    #1 indentation corrected - powered by cpplint!

--- src/TimeRequestHandlerFactory.cpp
+++ src/TimeRequestHandlerFactory.cpp
@@ -1,12 +1,12 @@
 #include "TimeRequestHandlerFactory.h"
 
-HTTPRequestHandler* TimeRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request)
-	{
-		if (request.getURI() == "/") 
-			return new TimeRequestHandler (_format);
-	        else if (request.getURI() == "/hello")
-			return new HelloWorldRequestHandler (_format);
-		else if (request.getURI() == "/cat")
-			return new RandomCatRequestHandler (_format);
-		else return 0;
-	}
+HTTPRequestHandler* TimeRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request) {
+if (request.getURI() == "/")
+    return new TimeRequestHandler (_format);
+else if (request.getURI() == "/hello")
+    return new HelloWorldRequestHandler (_format);
+else if (request.getURI() == "/cat")
+    return new RandomCatRequestHandler (_format);
+else
+    return 0;
+}
