eb03982d55e41e8132f3413122939e333659b837
Mateusz <mateusz_poczta@o2.pl>
2019-04-23 20:42:00 +0200

    #1 indentation corrected

--- src/TimeRequestHandlerFactory.cpp
+++ src/TimeRequestHandlerFactory.cpp
@@ -2,11 +2,11 @@
 
 HTTPRequestHandler* TimeRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request)
 	{
-		if (request.getURI() == "/")
-            return new TimeRequestHandler (_format);
-        else if (request.getURI() == "/hello")
-            return new HelloWorldRequestHandler (_format);
-        else if (request.getURI() == "/cat")
-            return new RandomCatRequestHandler (_format);
+		if (request.getURI() == "/") 
+			return new TimeRequestHandler (_format);
+	        else if (request.getURI() == "/hello")
+			return new HelloWorldRequestHandler (_format);
+		else if (request.getURI() == "/cat")
+			return new RandomCatRequestHandler (_format);
 		else return 0;
 	}
