0ab02dee7a0092c91f1cd388a3cc510ec3d93c09
Mateusz <mateusz_poczta@o2.pl>
2019-04-23 20:23:40 +0200

    #1 implementation of create method moved from header to source file

--- include/TimeRequestHandlerFactory.h
+++ include/TimeRequestHandlerFactory.h
@@ -49,17 +49,7 @@ public:
 	{
 	}
 
-	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request)
-	{
-		if (request.getURI() == "/")
-			return new TimeRequestHandler(_format);
-        else if (request.getURI() == "/hello")
-			return new HelloWorldRequestHandler(_format);
-        else if (request.getURI() == "/cat")
-			return new RandomCatRequestHandler(_format);
-		else
-			return 0;
-	}
+	HTTPRequestHandler* createRequestHandler(const HTTPServerRequest& request);
 
 private:
 	std::string _format;
--- src/TimeRequestHandlerFactory.cpp
+++ src/TimeRequestHandlerFactory.cpp
@@ -1,2 +1,13 @@
 #include "TimeRequestHandlerFactory.h"
 
+HTTPRequestHandler* TimeRequestHandlerFactory::createRequestHandler(const HTTPServerRequest& request)
+	{
+		if (request.getURI() == "/")
+			return new TimeRequestHandler(_format);
+        else if (request.getURI() == "/hello")
+			return new HelloWorldRequestHandler(_format);
+        else if (request.getURI() == "/cat")
+			return new RandomCatRequestHandler(_format);
+		else
+			return 0;
+	}
