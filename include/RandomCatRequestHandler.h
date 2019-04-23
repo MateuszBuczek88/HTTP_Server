a8665101acae4cbac2f80596208535f9f32605eb
Mateusz <mateusz_poczta@o2.pl>
2019-04-23 16:00:43 +0200

    no cache feature

--- include/RandomCatRequestHandler.h
+++ include/RandomCatRequestHandler.h
@@ -50,14 +50,12 @@ public:
 
 		std::ostream& ostr = response.send();
 		ostr << "<html><head><title>HTTPTimeServer powered by POCO C++ Libraries</title>";
-        ostr <<	"<meta http-equiv=\"Pragma\" content=\"no-cache\">";
-        ostr <<	"<meta http-equiv=\"Expires\" content=\"-1\">";
-        ostr <<	"<meta http-equiv=\"CACHE-CONTROL\" content=\"NO-CACHE\">";
-		ostr << "<meta http-equiv=\"refresh\" content=\"1\"></head>";
+
+		ostr << "<meta http-equiv=\"refresh\" content=\"2\"></head>";
 		ostr << "<body><p style=\"text-align: center; font-size: 48px;\">";
 		ostr << "My Work Is Done Here";
 		ostr << "<br>";
-		ostr << "<img src=https://cataas.com/c>";
+		ostr << "<img src=https://cataas.com/cat?" <<now.epochTime()<<">";
 		ostr << "</body></html>";
 
 	}
