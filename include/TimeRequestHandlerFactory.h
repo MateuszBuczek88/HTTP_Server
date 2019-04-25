64aa6e9867866d3105a75cdc4e80ea6c06e363df
MateuszBuczek88 <47189440+MateuszBuczek88@users.noreply.github.com>
2019-04-25 13:09:27 +0200

    #11 Refactoring

--- include/TimeRequestHandlerFactory.h
+++ include/TimeRequestHandlerFactory.h
@@ -2,43 +2,14 @@
 #define INCLUDE_TIMEREQUESTHANDLERFACTORY_H_
 #include <iostream>
 #include <string>
-#include "Poco/Net/HTTPServer.h"
-#include "Poco/Net/HTTPRequestHandler.h"
 #include "Poco/Net/HTTPRequestHandlerFactory.h"
-#include "Poco/Net/HTTPServerRequest.h"
-#include "Poco/Net/HTTPServerResponse.h"
-#include "Poco/Net/HTTPServerParams.h"
-#include "Poco/Net/ServerSocket.h"
-#include "Poco/Timestamp.h"
-#include "Poco/DateTimeFormatter.h"
-#include "Poco/DateTimeFormat.h"
-#include "Poco/Exception.h"
-#include "Poco/ThreadPool.h"
-#include "Poco/Util/ServerApplication.h"
-#include "Poco/Util/Option.h"
-#include "Poco/Util/OptionSet.h"
-#include "Poco/Util/HelpFormatter.h"
 #include "TimeRequestHandler.h"
 #include "HelloWorldRequestHandler.h"
 #include "RandomCatRequestHandler.h"
-#include "TimeRequestHandlerFactory.h"
 
-using Poco::Net::ServerSocket;
 using Poco::Net::HTTPRequestHandler;
 using Poco::Net::HTTPRequestHandlerFactory;
-using Poco::Net::HTTPServer;
 using Poco::Net::HTTPServerRequest;
-using Poco::Net::HTTPServerResponse;
-using Poco::Net::HTTPServerParams;
-using Poco::Timestamp;
-using Poco::DateTimeFormatter;
-using Poco::DateTimeFormat;
-using Poco::ThreadPool;
-using Poco::Util::ServerApplication;
-using Poco::Util::Application;
-using Poco::Util::Option;
-using Poco::Util::OptionSet;
-using Poco::Util::HelpFormatter;
 
 class TimeRequestHandlerFactory: public HTTPRequestHandlerFactory {
  public:
--- main.cpp
+++ main.cpp
@@ -1,22 +1,10 @@
-//
-// HTTPTimeServer.cpp
-//
-// This sample demonstrates the HTTPServer and related classes.
-//
-// Copyright (c) 2005-2006, Applied Informatics Software Engineering GmbH.
-// and Contributors.
-//
-// SPDX-License-Identifier:	BSL-1.0
-//
-
-
+#include <iostream>
 #include "Poco/Net/HTTPServer.h"
 #include "Poco/Net/HTTPRequestHandler.h"
 #include "Poco/Net/HTTPRequestHandlerFactory.h"
 #include "Poco/Net/HTTPServerParams.h"
 #include "Poco/Net/HTTPServerRequest.h"
 #include "Poco/Net/HTTPServerResponse.h"
-#include "Poco/Net/HTTPServerParams.h"
 #include "Poco/Net/ServerSocket.h"
 #include "Poco/Timestamp.h"
 #include "Poco/DateTimeFormatter.h"
@@ -31,8 +19,6 @@
 #include "HelloWorldRequestHandler.h"
 #include "RandomCatRequestHandler.h"
 #include "TimeRequestHandlerFactory.h"
-#include <iostream>
-
 
 using Poco::Net::ServerSocket;
 using Poco::Net::HTTPRequestHandler;
@@ -52,132 +38,98 @@ using Poco::Util::OptionSet;
 using Poco::Util::HelpFormatter;
 
 
-class HTTPTimeServer: public Poco::Util::ServerApplication
-	/// The main application class.
-	///
-	/// This class handles command-line arguments and
-	/// configuration files.
-	/// Start the HTTPTimeServer executable with the help
-	/// option (/help on Windows, --help on Unix) for
-	/// the available command line options.
-	///
-	/// To use the sample configuration file (HTTPTimeServer.properties),
-	/// copy the file to the directory where the HTTPTimeServer executable
-	/// resides. If you start the debug version of the HTTPTimeServer
-	/// (HTTPTimeServerd[.exe]), you must also create a copy of the configuration
-	/// file named HTTPTimeServerd.properties. In the configuration file, you
-	/// can specify the port on which the server is listening (default
-	/// 9980) and the format of the date/time string sent back to the client.
-	///
-	/// To test the TimeServer you can use any web browser (http://localhost:9980/).
-{
-public:
-	HTTPTimeServer(): _helpRequested(false)
-	{
-	}
+class HTTPTimeServer: public Poco::Util::ServerApplication {
+ public:
+    HTTPTimeServer(): _helpRequested(false) {}
+    ~HTTPTimeServer() {}
 
-	~HTTPTimeServer()
-	{
-	}
+ protected:
+    void initialize(Application& self) {
+        loadConfiguration();
+        ServerApplication::initialize(self);
+    }
 
-protected:
-	void initialize(Application& self)
-	{
-		loadConfiguration(); // load default configuration files, if present
-		ServerApplication::initialize(self);
-	}
+    void uninitialize() {
+        ServerApplication::uninitialize();
+    }
 
-	void uninitialize()
-	{
-		ServerApplication::uninitialize();
-	}
+    void defineOptions(OptionSet& options) {
+        ServerApplication::defineOptions(options);
+        options.addOption(
+            Option("help", "h", "display help information on command line arguments")
+                .required(false)
+                .repeatable(false));
+        options.addOption(
+            Option("config", "c", "use config.ini file instead of default params")
+                .required(false)
+                .repeatable(false)
+                .argument("directory", false));
+    }
 
-	void defineOptions(OptionSet& options)
-	{
-		ServerApplication::defineOptions(options);
+    void handleOption(const std::string& name, const std::string& value) {
+        ServerApplication::handleOption(name, value);
 
-		options.addOption(
-			Option("help", "h", "display help information on command line arguments")
-				.required(false)
-				.repeatable(false));
-       		 options.addOption(
-			Option("config", "c", "use config.ini file instead of default params")
-				.required(false)
-				.repeatable(false)
-				.argument("directory",false));
-	}
+        if (name == "help") {
+            _helpRequested = true;
+        }
+        if (name == "config") {
+            config_path = value;
+        }
+    }
 
-	void handleOption(const std::string& name, const std::string& value)
-	{
-		ServerApplication::handleOption(name, value);
+    void displayHelp() {
+        HelpFormatter helpFormatter(options());
+        helpFormatter.setCommand(commandName());
+        helpFormatter.setUsage("OPTIONS");
+        helpFormatter.setHeader("A web server that serves the current date and time.");
+        helpFormatter.format(std::cout);
+    }
 
-		if (name == "help")
-			_helpRequested = true;
-		if (name == "config"){
-            		 config_path = value;
-           	 }
-	}
+    int main(const std::vector<std::string>& args) {
+        if (_helpRequested) {
+            displayHelp();
+        } else {
+            if (!config_path.empty()) {
+                logger().information("Loading configuration file. Path: "+config_path);
+                try {
+                    loadConfiguration(config_path);
+                    logger().information("Config file " + config_path + " successfully loaded");
+                    }
+                catch (const Poco::InvalidArgumentException & e) {
+                    logger().fatal("Failed to load "+config_path);
+                }
+            }
 
-	void displayHelp()
-	{
-		HelpFormatter helpFormatter(options());
-		helpFormatter.setCommand(commandName());
-		helpFormatter.setUsage("OPTIONS");
-		helpFormatter.setHeader("A web server that serves the current date and time.");
-		helpFormatter.format(std::cout);
-	}
+            unsigned short port = (unsigned short) config().getInt("HTTPTimeServer.port" , 9980);
+            std::string format(config().getString("HTTPTimeServer.format", DateTimeFormat::SORTABLE_FORMAT));
+            int maxQueued  = config().getInt("HTTPTimeServer.maxQueued", 100);
+            int maxThreads = config().getInt("HTTPTimeServer.maxThreads", 16);
+            ThreadPool::defaultPool().addCapacity(maxThreads);
 
-	int main(const std::vector<std::string>& args)
-	{
-		if (_helpRequested)
-		{
-			displayHelp();
-		}
-		else
-		{
-			if ( ! config_path.empty () ) {
-				logger().information("Loading configuration file. Path: "+config_path);
-				try {
-					loadConfiguration(config_path);
-					logger().information("Config file " + config_path + " successfully loaded");
-				}
-				catch (const Poco::InvalidArgumentException & e) {
-					logger().fatal("Failed to load "+config_path);
-				}
-			}
+            HTTPServerParams* pParams = new HTTPServerParams;
+            pParams->setMaxQueued(maxQueued);
+            pParams->setMaxThreads(maxThreads);
 
-			unsigned short port = (unsigned short) config().getInt("HTTPTimeServer.port",9980);
-			std::string format(config().getString("HTTPTimeServer.format", DateTimeFormat::SORTABLE_FORMAT));
-			int maxQueued  = config().getInt("HTTPTimeServer.maxQueued", 100);
-			int maxThreads = config().getInt("HTTPTimeServer.maxThreads", 16);
-			ThreadPool::defaultPool().addCapacity(maxThreads);
+            ServerSocket svs(port);
 
-			HTTPServerParams* pParams = new HTTPServerParams;
-			pParams->setMaxQueued(maxQueued);
-			pParams->setMaxThreads(maxThreads);
+            HTTPServer srv(new TimeRequestHandlerFactory(format), svs, pParams);
 
-			// set-up a server socket
-			ServerSocket svs(port);
-			// set-up a HTTPServer instance
-			HTTPServer srv(new TimeRequestHandlerFactory(format), svs, pParams);
-			// start the HTTPServer
-			srv.start();
-			// wait for CTRL-C or kill
-			waitForTerminationRequest();
-			// Stop the HTTPServer
-			srv.stop();
-		}
-		return Application::EXIT_OK;
-	}
+            srv.start();
 
-private:
-	bool _helpRequested;
-	std ::string config_path;
+            waitForTerminationRequest();
+
+            srv.stop();
+        }
+        return Application::EXIT_OK;
+    }
+
+ private:
+    bool _helpRequested;
+    std ::string config_path;
 };
 
 
-int main(int argc, char** argv)
-{
-	HTTPTimeServer app;
-	return app.run(argc, argv);
+int main(int argc, char** argv) {
+    HTTPTimeServer app;
+    return app.run(argc, argv);
 }
