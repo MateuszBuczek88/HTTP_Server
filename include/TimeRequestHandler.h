2679d110e6264e24c992dfb526997f3f995317e8
Mateusz <mateusz_poczta@o2.pl>
2019-04-25 12:44:13 +0200

    #11 refactoring

--- include/TimeRequestHandler.h
+++ include/TimeRequestHandler.h
@@ -2,27 +2,16 @@
 #define INCLUDE_TIMEREQUESTHANDLER_H_
 #include <iostream>
 #include <string>
-#include "Poco/Net/HTTPServer.h"
 #include "Poco/Net/HTTPRequestHandler.h"
-#include "Poco/Net/HTTPRequestHandlerFactory.h"
 #include "Poco/Net/HTTPServerRequest.h"
 #include "Poco/Net/HTTPServerResponse.h"
-#include "Poco/Net/HTTPServerParams.h"
-#include "Poco/Net/ServerSocket.h"
 #include "Poco/Timestamp.h"
 #include "Poco/DateTimeFormatter.h"
 #include "Poco/DateTimeFormat.h"
-#include "Poco/Exception.h"
-#include "Poco/ThreadPool.h"
 #include "Poco/Util/ServerApplication.h"
-#include "Poco/Util/Option.h"
-#include "Poco/Util/OptionSet.h"
-#include "Poco/Util/HelpFormatter.h"
+
 
 using Poco::Net::HTTPRequestHandler;
-using Poco::Net::HTTPServer;
-using Poco::Net::HTTPRequestHandler;
-using Poco::Net::HTTPRequestHandlerFactory;
 using Poco::Net::HTTPServerRequest;
 using Poco::Net::HTTPServerResponse;
 using Poco::Util::Application;
